6fcee7fb9ab4a22bc8c147b161f4ee008bf05483
Dion Theroulde <dionjr105@gmail.com>
2021-05-27 14:17:52 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+//const getOut = document.querySelector('main')
+//getOut.parentNode.removeChild(getOut)
+document.querySelector('main#main').remove()
+const newHeader = document.querySelector('h1#victory')
